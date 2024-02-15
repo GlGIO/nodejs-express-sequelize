8121315ed5aab9123678d97db793921450057978
Giovanni Sandrini <sandrini.dev@gmail.com>
2024-02-15 17:14:40 -0300

    aula 1

---
+++ src/services/Services.js
@@ -0,0 +1,16 @@
+const database = require("../models");
+
+class Services {
+	constructor(nomeDoModel) {
+		this.model = nomeDoModel;
+	}
+	async pegaTodosOsRegistros() {
+		try {
+			return await database[this.model].findAll();
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+}
+
+module.exports = Services;
